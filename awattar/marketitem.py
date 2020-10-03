691376623a71035d6c51ea17672ef2916367fcba
Peter Gransdorfer <peter.gransdorfer@siconnex.com>
2020-10-03 18:33:13 +0200

    store time in datetime data type

--- awattar/marketitem.py
+++ awattar/marketitem.py
@@ -3,34 +3,59 @@ from dateutil import tz
 
 class MarketItem(object):
     def __init__(self,
-                 start_timestamp,
-                 end_timestamp,
+                 start_datetime : datetime,
+                 end_datetime : datetime,
                  marketprice,
                  unit
                  ):
         """Construct a new aWATTarClient item object."""
 
-        self._start_timestamp = start_timestamp
-        self._end_timestamp = end_timestamp
+        self._start_datetime = start_datetime 
+        self._end_datetime = end_datetime
         self._marketprice = marketprice
         self._unit = unit
+
+    @classmethod
+    def by_timestamp(cls,
+                 start_timestamp,
+                 end_timestamp,
+                 marketprice,
+                 unit
+                 ):
+        """
+        Create new instance with timestamp
+
+        Parameters
+        ----------
+        start_timestamp
+            Start timestamp
+        end_timestamp
+            End timestamp        
+
+        """  
+
+        return cls(
+            datetime.datetime.utcfromtimestamp(start_timestamp / 1000).replace(tzinfo=datetime.timezone.utc),
+            datetime.datetime.utcfromtimestamp(end_timestamp / 1000).replace(tzinfo=datetime.timezone.utc),
+            marketprice,
+            unit)
+
+        self._start_datetime = datetime.datetime.utcfromtimestamp(start_timestamp / 1000).replace(tzinfo=datetime.timezone.utc) 
+        self._end_datetime = datetime.datetime.utcfromtimestamp(end_timestamp / 1000).replace(tzinfo=datetime.timezone.utc) 
+        self._marketprice = marketprice
+        self._unit = unit     
 
     @property
     def start_datetime(self):
 
-        #get datetime in utc
-        value = datetime.datetime.utcfromtimestamp(self._start_timestamp / 1000).replace(tzinfo=datetime.timezone.utc) 
-
         #return in local timezone
-        return value.astimezone(tz.tzlocal()) 
+        return self._start_datetime.astimezone(tz.tzlocal()) 
     
     @property
     def end_datetime(self):
-        #get datetime in utc
-        value = datetime.datetime.utcfromtimestamp(self._end_timestamp / 1000).replace(tzinfo=datetime.timezone.utc) 
 
         #return in local timezone
-        return value.astimezone(tz.tzlocal()) 
+        return self._end_datetime.astimezone(tz.tzlocal()) 
 
     @property
     def marketprice(self):
