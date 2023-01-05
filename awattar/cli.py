65a827b9d55163d6e551e79a9afee815d57fe5ec
jofrev <jonathan@vahlsing.de>
2023-01-05 16:25:08 +0100

    Add options for start and end of data timeframe

--- awattar/cli.py
+++ awattar/cli.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import dataclasses
+from typing import Optional
 
 import click
 from dateutil import tz
@@ -28,12 +29,16 @@ def cli(ctx, country):
 
 @cli.command
 @click.pass_obj
-def fetch(obj: CliContext):
+@click.option("--start", type=click.DateTime(), default=None)
+@click.option("--end", type=click.DateTime(), default=None)
+def fetch(obj: CliContext, start: Optional[datetime.datetime], end: Optional[datetime.datetime]):
     """Fetch hourly energy prices"""
     today = datetime.date.today()
-    start: datetime.datetime = datetime.datetime.combine(
-        today, datetime.time.min, tz.tzlocal()
-    )
-    end: datetime.datetime = datetime.timedelta(1) + start
+    if not start:
+        start = datetime.datetime.combine(
+            today, datetime.time.min, tz.tzlocal()
+        )
+    if not end:
+        end = datetime.timedelta(1) + start
     items = obj.client.request(start, end)
     print(json.dumps([item.to_json_dict() for item in items], indent=4))
