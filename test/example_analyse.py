0d07101f4d45936ee6ebac0da61e217bd0747c4e
Peter Gransdorfer <peter.gransdorfer@cattronix.com>
2023-11-10 16:15:28 +0100

    code style

--- test/example_analyse.py
+++ test/example_analyse.py
@@ -1,91 +1,86 @@
-import sys
-
 import datetime
+import statistics
+import sys
 from datetime import timedelta
-import statistics 
+
 import numpy
+
 sys.path.append("..")
 from awattar.client import AwattarClient
 
+
 def main(argv):
+    print("Connect to aWATTar")
+    client = AwattarClient("AT")
 
-    print ('Connect to aWATTar')
-    client = AwattarClient('AT')
-
-    print ('Get Market data from API - Today')
+    print("Get Market data from API - Today")
     data = client.today()
     for item in data:
-        print(f'{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}')    
+        print(f"{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}")
 
-    print ('Get Market data from API - Tomorrow')
+    print("Get Market data from API - Tomorrow")
     data = client.tomorrow()
     for item in data:
-        print(f'{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}')    
+        print(f"{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}")
 
-    print ('Get Market data from API')
+    print("Get Market data from API")
     data = client.request()
     for item in data:
-        print(f'{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}')            
+        print(f"{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh {type(item.marketprice)}")
 
-    
     min_item = client.min()
-    print(f'Min: {min_item.start_datetime:%Y-%m-%d %H:%M:%S} - {min_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(min_item.marketprice / 1000):.4f} EUR/kWh')
+    print(f"Min: {min_item.start_datetime:%Y-%m-%d %H:%M:%S} - {min_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(min_item.marketprice / 1000):.4f} EUR/kWh")
 
     max_item = client.max()
-    print(f'Max: {max_item.start_datetime:%Y-%m-%d %H:%M:%S} - {max_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(max_item.marketprice / 1000):.4f} EUR/kWh')
+    print(f"Max: {max_item.start_datetime:%Y-%m-%d %H:%M:%S} - {max_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(max_item.marketprice / 1000):.4f} EUR/kWh")
 
     mean_item = client.mean()
-    print(f'Mean: {mean_item.start_datetime:%Y-%m-%d %H:%M:%S} - {mean_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(mean_item.marketprice / 1000):.4f} EUR/kWh')
-
+    print(f"Mean: {mean_item.start_datetime:%Y-%m-%d %H:%M:%S} - {mean_item.end_datetime:%Y-%m-%d %H:%M:%S} - {(mean_item.marketprice / 1000):.4f} EUR/kWh")
 
     best_slot = client.best_slot(3)
     if best_slot is None:
         print("No slot found")
-    else:        
-        print(f'Best slot1: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh')
+    else:
+        print(f"Best slot1: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh")
 
-    best_slot = client.best_slot(1,datetime.datetime(2020, 10, 17, 12, 0, 0),datetime.datetime(2020, 10, 17, 18, 0, 0))
+    best_slot = client.best_slot(1, datetime.datetime(2020, 10, 17, 12, 0, 0), datetime.datetime(2020, 10, 17, 18, 0, 0))
     if best_slot is None:
         print("No slot found")
-    else:        
-        print(f'Best slot2: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh')
+    else:
+        print(f"Best slot2: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh")
 
     starttime_slot = datetime.datetime.now()
-    starttime_slot = starttime_slot.replace(hour=12)	
+    starttime_slot = starttime_slot.replace(hour=12)
     endtime_slot = starttime_slot + timedelta(hours=6)
 
-    best_slot = client.best_slot(1,starttime_slot,endtime_slot)
+    best_slot = client.best_slot(1, starttime_slot, endtime_slot)
     if best_slot is None:
         print("No slot found")
-    else:        
-        print(f'Best slot3: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh')
+    else:
+        print(f"Best slot3: {best_slot.start_datetime:%Y-%m-%d %H:%M:%S} - {best_slot.end_datetime:%Y-%m-%d %H:%M:%S} - {(best_slot.marketprice / 1000):.4f} EUR/kWh")
 
- 
+    # print ('Get Market data from API')
+    # data = client.request()
 
-    #print ('Get Market data from API')
-    #data = client.request()
-    
-    #maxitem = max(data, key=lambda p: p.marketprice)
+    # maxitem = max(data, key=lambda p: p.marketprice)
 
-    #print(f'Max: {maxitem.start_datetime:%Y-%m-%d %H:%M:%S} - {maxitem.end_datetime:%Y-%m-%d %H:%M:%S} - {(maxitem.marketprice / 1000):.4f} EUR/kWh')
+    # print(f'Max: {maxitem.start_datetime:%Y-%m-%d %H:%M:%S} - {maxitem.end_datetime:%Y-%m-%d %H:%M:%S} - {(maxitem.marketprice / 1000):.4f} EUR/kWh')
 
-    #minitem = min(data, key=lambda p: p.marketprice)
+    # minitem = min(data, key=lambda p: p.marketprice)
 
-    #print(f'Min: {minitem.start_datetime:%Y-%m-%d %H:%M:%S} - {minitem.end_datetime:%Y-%m-%d %H:%M:%S} - {(minitem.marketprice / 1000):.4f} EUR/kWh')
+    # print(f'Min: {minitem.start_datetime:%Y-%m-%d %H:%M:%S} - {minitem.end_datetime:%Y-%m-%d %H:%M:%S} - {(minitem.marketprice / 1000):.4f} EUR/kWh')
 
+    # i_want_object_of_test = [test(test_obj.val.lower()) for test_obj in test_lst]
+    # attr=(data, key=lambda p: p.marketprice)
+    # print(vars(attr))
+    # medianitem = numpy.median(attr)
 
+    # print(f'Median: {medianitem} EUR/kWh')
 
-    #i_want_object_of_test = [test(test_obj.val.lower()) for test_obj in test_lst]
-    #attr=(data, key=lambda p: p.marketprice)
-    #print(vars(attr))
-    #medianitem = numpy.median(attr)
-
-    #print(f'Median: {medianitem} EUR/kWh')
-
-    #List of data
-    #for item in data:
+    # List of data
+    # for item in data:
     #    print(f'{item.start_datetime:%Y-%m-%d %H:%M:%S} - {item.end_datetime:%Y-%m-%d %H:%M:%S} - {(item.marketprice / 1000):.4f} EUR/kWh')
-         
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,75 +1,73 @@
-import pytest
-import click
 import datetime
-import pytz
-from datetime import timedelta
+
 from tzlocal import get_localzone
 
 from awattar.client import AwattarClient
 from awattar.marketitem import MarketItem
 
-def test_basic_api_today():
-    client = AwattarClient('AT')
+
+def test_basic_api_today() -> None:
+    client = AwattarClient("AT")
     data = client.today()
 
     assert len(data) == 23
-    assert data[0].start_datetime == datetime.datetime.now().replace(hour=1, minute=0,second=0,microsecond=0, tzinfo=get_localzone())
+    assert data[0].start_datetime == datetime.datetime.now(tz=datetime.timezone.utc).replace(hour=1, minute=0, second=0, microsecond=0, tzinfo=get_localzone())
 
-def test_basic_api_past():
-    client = AwattarClient('AT')    
-    data = client.request(datetime.datetime(2023, 10, 17, 12, 0, 0),datetime.datetime(2023, 10, 17, 18, 0, 0))
+
+def test_basic_api_past() -> None:
+    client = AwattarClient("AT")
+    data = client.request(datetime.datetime(2023, 10, 17, 12, 0, 0, tzinfo=get_localzone()), datetime.datetime(2023, 10, 17, 18, 0, 0, tzinfo=get_localzone()))
 
     assert len(data) == 6
-    assert data[0].start_datetime == datetime.datetime(2023, 10, 17, 12, 0, 0, tzinfo=get_localzone()) 
+    assert data[0].start_datetime == datetime.datetime(2023, 10, 17, 12, 0, 0, tzinfo=get_localzone())
 
-    #test negative results; wrong timerange
-    best_slot = client.best_slot(1,datetime.datetime(2020, 10, 17, 12, 0, 0),datetime.datetime(2020, 10, 17, 18, 0, 0))
+    # test negative results; wrong timerange
+    best_slot = client.best_slot(1, datetime.datetime(2020, 10, 17, 12, 0, 0, tzinfo=get_localzone()), datetime.datetime(2020, 10, 17, 18, 0, 0, tzinfo=get_localzone()))
     assert best_slot is None
 
     # get best slot
-    best_slot = client.best_slot(1,datetime.datetime(2023, 10, 17, 12, 0, 0),datetime.datetime(2023, 10, 17, 18, 0, 0))  
-    assert isinstance(best_slot, MarketItem)  
+    best_slot = client.best_slot(1, datetime.datetime(2023, 10, 17, 12, 0, 0, tzinfo=get_localzone()), datetime.datetime(2023, 10, 17, 18, 0, 0, tzinfo=get_localzone()))
+    assert isinstance(best_slot, MarketItem)
     assert best_slot.marketprice == 125.35
 
-def test_basic_api_past2():
-    client = AwattarClient('AT')    
-    data = client.request(datetime.datetime(2023, 10, 17, 3, 0, 0),datetime.datetime(2023, 10, 17, 18, 0, 0))
 
-    print(data)
+def test_basic_api_past2() -> None:
+    client = AwattarClient("AT")
+    data = client.request(datetime.datetime(2023, 10, 17, 3, 0, 0, tzinfo=get_localzone()), datetime.datetime(2023, 10, 17, 18, 0, 0, tzinfo=get_localzone()))
 
     assert len(data) == 15
 
-    assert data[0].start_datetime == datetime.datetime(2023, 10, 17, 3, 0, 0, tzinfo=get_localzone()) 
-    assert data[0].end_datetime == datetime.datetime(2023, 10, 17, 4, 0, 0, tzinfo=get_localzone()) 
+    assert data[0].start_datetime == datetime.datetime(2023, 10, 17, 3, 0, 0, tzinfo=get_localzone())
+    assert data[0].end_datetime == datetime.datetime(2023, 10, 17, 4, 0, 0, tzinfo=get_localzone())
     assert data[0].energy_unit == "MWh"
     assert data[0].price_per_kWh == 0.10409
     assert data[0].marketprice == 104.09
     assert data[0].unit == "Eur/MWh"
 
-    assert data[3].start_datetime == datetime.datetime(2023, 10, 17, 6, 0, 0, tzinfo=get_localzone()) 
-    assert data[3].end_datetime == datetime.datetime(2023, 10, 17, 7, 0, 0, tzinfo=get_localzone())     
+    assert data[3].start_datetime == datetime.datetime(2023, 10, 17, 6, 0, 0, tzinfo=get_localzone())
+    assert data[3].end_datetime == datetime.datetime(2023, 10, 17, 7, 0, 0, tzinfo=get_localzone())
     assert data[3].energy_unit == "MWh"
     assert data[3].price_per_kWh == 0.15369999999999998
     assert data[3].marketprice == 153.7
-    assert data[3].unit == "Eur/MWh"    
-    
-    assert data[6].start_datetime == datetime.datetime(2023, 10, 17, 9, 0, 0, tzinfo=get_localzone()) 
-    assert data[6].end_datetime == datetime.datetime(2023, 10, 17, 10, 0, 0, tzinfo=get_localzone())     
+    assert data[3].unit == "Eur/MWh"
+
+    assert data[6].start_datetime == datetime.datetime(2023, 10, 17, 9, 0, 0, tzinfo=get_localzone())
+    assert data[6].end_datetime == datetime.datetime(2023, 10, 17, 10, 0, 0, tzinfo=get_localzone())
     assert data[6].energy_unit == "MWh"
     assert data[6].price_per_kWh == 0.15925999999999998
     assert data[6].marketprice == 159.26
     assert data[6].unit == "Eur/MWh"
 
-    assert data[10].start_datetime == datetime.datetime(2023, 10, 17, 13, 0, 0, tzinfo=get_localzone()) 
-    assert data[10].end_datetime == datetime.datetime(2023, 10, 17, 14, 0, 0, tzinfo=get_localzone()) 
+    assert data[10].start_datetime == datetime.datetime(2023, 10, 17, 13, 0, 0, tzinfo=get_localzone())
+    assert data[10].end_datetime == datetime.datetime(2023, 10, 17, 14, 0, 0, tzinfo=get_localzone())
     assert data[10].energy_unit == "MWh"
     assert data[10].price_per_kWh == 0.11425
-    assert data[10].marketprice == 114.25    
+    assert data[10].marketprice == 114.25
     assert data[10].unit == "Eur/MWh"
 
-    assert data[14].start_datetime == datetime.datetime(2023, 10, 17, 17, 0, 0, tzinfo=get_localzone()) 
-    assert data[14].end_datetime == datetime.datetime(2023, 10, 17, 18, 0, 0, tzinfo=get_localzone()) 
+    assert data[14].start_datetime == datetime.datetime(2023, 10, 17, 17, 0, 0, tzinfo=get_localzone())
+    assert data[14].end_datetime == datetime.datetime(2023, 10, 17, 18, 0, 0, tzinfo=get_localzone())
     assert data[14].energy_unit == "MWh"
     assert data[14].price_per_kWh == 0.16490000000000002
-    assert data[14].marketprice == 164.9    
+    assert data[14].marketprice == 164.9
     assert data[14].unit == "Eur/MWh"
