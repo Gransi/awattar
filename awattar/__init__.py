a0541c3fa040b08ca69746388167e78c93e35061
Peter Gransdorfer <peter.gransdorfer@cattronix.com>
2023-11-10 15:57:36 +0100

    fix ruff erros

--- awattar/__init__.py
+++ awattar/__init__.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
 """Initialize the aWATTar package."""
 
 from .client import AwattarClient
-from .marketitem import MarketItem
-from .cli import cli as _cli    # for CLI entry only
 
 __all__ = [
-    'AwattarClient'
+    'AwattarClient',
 ]
 
 __version__ = '0.2.2'
